e48ca4f0fffbd94d1bf040d2bd8274705406b03b
Lauta2217 <94128514+Lauta2217@users.noreply.github.com>
2024-10-29 18:26:16 -0300

    Add files via upload

--- repaso_parcial_2/repaso_arbolBB.py
+++ repaso_parcial_2/repaso_arbolBB.py
@@ -27,183 +27,179 @@ class Arbol_binario_busqueda:
         return self.__raiz
         
     def insertar(self,valor,actual):
-        nodo = Nodo(valor)
-        if self.__raiz == None:
-            self.__raiz = nodo
-            return
+        nodo = Nodo(valor) #creo el nodo con el valor ingresado
+        if self.__raiz == None: # si no hay raiz 
+            self.__raiz = nodo #se define como raiz el valor ingresado
+            return # me vuelvo
         else:
-            if actual.get_value() > valor:
-                if actual.get_left() is None:
-                    actual.set_left(nodo)
-                    return
+            if actual.get_value() > valor: #si el valor del actual es mayor que el valor que quiero ingresar
+                if actual.get_left() is None: #y si a la izquierda no tiene nada
+                    actual.set_left(nodo) #entonces inserto ese valor a su izquierda ya que es mas pequeño
+                    return # me vuelvo
                 else:
-                    self.insertar(valor,actual.get_left())
-            elif actual.get_value() < valor:
-                if actual.get_right() is None:
-                    actual.set_right(nodo)
-                    return
+                    self.insertar(valor,actual.get_left()) #si tiene algo a la izquierda entonces vuelvo a iterar, pero con ese valor que tiene a la izquierda
+            elif actual.get_value() < valor: #si el valor del actual es menor que el valor que quiero ingresar
+                if actual.get_right() is None: #y si a la derecha no tiene nada
+                    actual.set_right(nodo) #entonces inserto ese valor a su derecha ya que es mas grande
+                    return # me vuelvo
                 else:
-                    self.insertar(valor,actual.get_right())
+                    self.insertar(valor,actual.get_right()) #si tiene algo a la derecha entonces vuelvo a iterar, pero con ese valor que tiene a la derecha
     def eliminar(self, valor, actual):
-        if actual is None:
-            return actual
+        if actual is None: #si no hay actual
+            return actual #entonces lo retorno (es lo mismo que poner return None)
         # Buscar el nodo a eliminar
-        if valor < actual.get_value():
-            actual.set_left(self.eliminar(valor, actual.get_left()))
-        elif valor > actual.get_value():
-            actual.set_right(self.eliminar(valor, actual.get_right()))
+        if valor < actual.get_value():  #si el valor del actual es mayor que el valor que quiero eliminar
+            actual.set_left(self.eliminar(valor, actual.get_left())) # a la izquierda va a quedar lo que me retorne la funcion
+        elif valor > actual.get_value(): #si el valor del actual es mayor que el valor que quiero eliminar
+            actual.set_right(self.eliminar(valor, actual.get_right())) # a la derecha va a quedar lo que me retorne la funcion
         else:
             # Caso 1: Nodo sin hijos
-            if actual.get_left() is None and actual.get_right() is None:
+            if actual.get_left() is None and actual.get_right() is None: #si no tiene hijos tonce lo cambio a none al nodo a eliminar
                 return None
             # Caso 2: Nodo con un solo hijo
-            elif actual.get_left() is None:
+            elif actual.get_left() is None: #si no tiene un valor a su izquierda entonces retorno el de la derecha
                 return actual.get_right()
-            elif actual.get_right() is None:
+            elif actual.get_right() is None: #si no tiene un valor a su derecga entonces retorno el de la izquierda
                 return actual.get_left()
             # Caso 3: Nodo con dos hijos
-            else:
-                # Encontrar el sucesor inorden (mínimo en el subárbol derecho)
-                sucesor_inorden = self.min_valor_nodo(actual.get_right())
-                # Reemplazar el valor del nodo actual con el valor del sucesor
-                actual.set_value(sucesor_inorden.get_left().get_value()) 
-                # Eliminar el sucesor inorden en el subárbol derecho
-                sucesor_inorden.set_left(sucesor_inorden.get_left().get_right())
-        return actual
+            else: #si no tiene ni un valor a la izquierda ni un valor a la derecha
+                sucesor_inorden = self.min_valor_nodo(actual.get_right()) # Busco el padre del nodo mas pequeño en el subárbol derecho
+                actual.set_value(sucesor_inorden.get_left().get_value()) # Reemplazo el valor del nodo actual con el valor del sucesor
+                sucesor_inorden.set_left(sucesor_inorden.get_left().get_right()) # como subi un nodo del arbol para reemplazar el eliminado entonces no puedo perder lo que tenia hacia abajo el nodo que sirve de reemplazo, por eso es este enganche
+        return actual #Retorno el nodo eliminado
     def min_valor_nodo(self, nodo):
-        # Bajar por la izquierda hasta encontrar el nodo más pequeño
-        while nodo.get_left().get_left() is not None:
+        while nodo.get_left().get_left() is not None:# Bajar por la izquierda hasta encontrar el nodo anterior al nodo más pequeño
             nodo = nodo.get_left()
-        return nodo
+        return nodo #lo retorno
     def buscar(self,valor,actual):
-        if actual is None:
-            band = False
-        elif actual.get_value() == valor:
-            band = True
-        elif actual.get_value() > valor:
-                band = self.buscar(valor,actual.get_left())
-        else:
-                band = self.buscar(valor,actual.get_right())
-        return band  
-    def nivel(self,valor,actual,nivel = 0): #nivel del arbol
-        if actual is None:
+        if actual is None: #si actual es none entonces no lo encontré
+            band = False # retorno false
+        elif actual.get_value() == valor: # si el valor del actual es el que estoy buscando retorno
+            band = True #retorno true
+        elif actual.get_value() > valor: #si el valor del actual es mayor al que estoy buscando me muevo la izquierda
+                band = self.buscar(valor,actual.get_left()) # bandera va a recibir ya sea un false o un true de las llamadas recursivas desde la izquierda
+        else: #si el valor del actual es menor al que estoy buscando me muevo la izquierda
+                band = self.buscar(valor,actual.get_right())# bandera va a recibir ya sea un false o un true de las llamadas recursivas desde la derecha
+        return band  # retorno ya sea true o false
+    def nivel(self,valor,actual,nivel = 0): #nivel del arbol tenemos un parametro nivel que ayuda en cada recursion a determianr el nivel del nodo
+        if actual is None: #si actual es none, entonces no encontré el valor
             print("No encontrado\n")
-            return
-        if actual.get_value() == valor:
+            return #Retorno
+        if actual.get_value() == valor: #si actual es el valor que estoy buscando entonces printeo su nivel
             print(f"{valor} tiene nivel: {nivel}\n")
-        elif actual.get_value() > valor:
-                self.nivel(valor,actual.get_left(),nivel+1)
-        else:
-                self.nivel(valor,actual.get_right(),nivel+1)
+        elif actual.get_value() > valor:#si el valor del actual es mayor al que estoy buscando me muevo la izquierda
+                self.nivel(valor,actual.get_left(),nivel+1) #me voy para la izquierda y aumento el nivel
+        else:#si el valor del actual es menor al que estoy buscando me muevo la izquierda
+                self.nivel(valor,actual.get_right(),nivel+1)  #me voy para la derecha y aumento el nivel
                 
     def hoja(self,valor,actual):
-        if actual is None:
+        if actual is None: #si actual es none, entonces no encontré el valor
             print("No encontrado\n")
             band = False
-        if actual.get_value() == valor:
-            if actual.get_left() is None and actual.get_right() is None:    
-                band = True
-            else:
-                band = False
-        elif actual.get_value() > valor:
-            band = self.hoja(valor,actual.get_left())
-        else:
-            band = self.hoja(valor,actual.get_right())
-        return band
+        if actual.get_value() == valor:  #si actual es el valor que estoy buscando entonces evaluo
+            if actual.get_left() is None and actual.get_right() is None:   #si no tiene nada la izq y nada a la der entonces es hoja
+                band = True #retorno true
+            else: #si tiene al menos un hijo
+                band = False #retorno false
+        elif actual.get_value() > valor: #si el valor del actual es mayor al que estoy buscando me muevo la izquierda
+            band = self.hoja(valor,actual.get_left()) #me voy para la izquierda y recibo el valor de la recursion 
+        else:#si el valor del actual es menor al que estoy buscando me muevo la izquierda
+            band = self.hoja(valor,actual.get_right()) #me voy para la derecha y recibo el valor de la recursion 
+        return band #retorno band
     
     def hijo(self,hijo,supuesto_padre,actual):
         band = False
-        if actual is None:
+        if actual is None: #si actual es none, entonces no encontré el valor
             band = False
-        if supuesto_padre == actual.get_value():
+        if supuesto_padre == actual.get_value(): #si el actual es el supuesto padre 
             print("viendo si tiene pendejos")
-            if actual.get_left() is not None:
-                if actual.get_left().get_value() == hijo:
-                    band = True
-            elif actual.get_right() is not None:
-                if actual.get_right().get_value() == hijo:
-                    band = True
-        elif actual.get_value() > supuesto_padre:
-             self.hijo(hijo,supuesto_padre,actual.get_left())
-        else:
-             self.hijo(hijo,supuesto_padre,actual.get_right())
-        return band
+            if actual.get_left() is not None: #si a la izquierda tiene algo evaluo
+                if actual.get_left().get_value() == hijo: #si lo que tiene a la izquierda es el hijo que estamos buscando
+                    band = True #band true #band true
+            elif actual.get_right() is not None: #sino si a la derecha tiene algo evaluo
+                if actual.get_right().get_value() == hijo: #si lo que tiene a la derecha es el hijo que estamos buscando
+                    band = True #band true      #si nada de eso pasa se retorna
+        elif actual.get_value() > supuesto_padre: #si el valor del actual es mayor al valor del padre que estoy buscando me muevo la izquierda
+             self.hijo(hijo,supuesto_padre,actual.get_left()) #me voy para la izquierda 
+        else: #si el valor del actual es menor al valor del padre que estoy buscando me muevo la derecha
+             self.hijo(hijo,supuesto_padre,actual.get_right())  #me voy para la izquierda 
+        return band #retorno band
     def camino(self,inicio,fin,actual,band = False,camino = []): #se puede usar la funcion buscar, pero en el parcial hay que definirla tambien
-        if actual is None:
-            return None
-        if actual.get_value() == inicio or band:
-            band = True
-            camino.append(actual.get_value())     
-            if actual.get_value() == fin:
+        if actual is None: #si actual es none, entonces no encontré el valor
+            return None #retorno None
+        if actual.get_value() == inicio or band: #si actual es el valor de inicio del camino o la bandera que me permite manejar la condicion de que ya tengo el inicio es true entonces entro
+            band = True #si solo ingresé por que el actual es igual al inicio entonces cambio la bandera para despues
+            camino.append(actual.get_value())  #inicio el camino con el actual    
+            if actual.get_value() == fin: #si el actual es igual al fin entonces retorno nomas
                 return
-            elif actual.get_value()>fin:
-                self.camino(inicio,fin,actual.get_left(),band,camino)
-            else:
-                self.camino(inicio,fin,actual.get_right(),band,camino)
-        elif actual.get_value()>inicio:
+            elif actual.get_value()>fin: #si el valor del actual es mayor al valor del fin que estoy buscando me muevo la izquierda
+                self.camino(inicio,fin,actual.get_left(),band,camino) #recursion para la izquierda, pero ya le agrego los parametros bandera y camino para no perderlos
+            else:#si el valor del actual es menor al valor del fin que estoy buscando me muevo la derecha
+                self.camino(inicio,fin,actual.get_right(),band,camino) #recursion para la derecha, pero ya le agrego los parametros bandera y camino para no perderlos
+        elif actual.get_value()>inicio: #si el valor del actual es mayor al inicio al valor del padre que estoy buscando me muevo la izquierda
             self.camino(inicio,fin,actual.get_left())
-        else:
+        else: #si el valor del actual es menor al inicio al valor del padre que estoy buscando me muevo la derecha
             self.camino(inicio,fin,actual.get_right())
-        if band:
+        if fin in camino: #si esta el fin en el camino tonce lo encontró
             return camino
-        else:
+        else: #no hay camino
             return None
         
     def altura(self,actual):
-        if actual is None:
+        if actual is None: #si actual es none retorno 0
             altura = 0
         else:
-            altura = 1 + max(self.altura(actual.get_left()),self.altura(actual.get_right()))
-        return altura
+            altura = 1 + max(self.altura(actual.get_left()),self.altura(actual.get_right())) #la altura es 1 + la maxima altura entre la izquierda y derecha
+        return altura #la retorno
     
     def padre_y_hermano(self,valor,actual):
-        if actual is None:
+        if actual is None: #si el actual es none retorno none
             return None
-        if actual.get_left().get_value() == valor or actual.get_left().get_value() == valor:
-            if actual.get_left().get_value() != valor:
+        if actual.get_left().get_value() == valor or actual.get_right().get_value() == valor: #si el valor que tiene a la izquierda el actual es el valor que estoy buscando o lo mismo pero a la derecha entonces encontré al padre
+            if actual.get_left().get_value() != valor: #si el valor del padre a la izquierda es distinto al ingresado tonce a la derecha  tiene hermano o no
                 print(f"Padre de {valor}: {actual.get_value()} y su hermano es: {actual.get_left().get_value() }")
-            else:
+            else: #si el valor del padre a la derecha es distinto al ingresado tonce a la izquierda tiene hermano o no
                 print(f"Padre de {valor}: {actual.get_value()} y su hermano es: {actual.get_right().get_value() }")
-        elif actual.get_value() > valor:
-            self.padre_y_hermano(valor,actual.get_left())
-        else:
-            self.padre_y_hermano(valor,actual.get_right())
-            
+        elif actual.get_value() > valor: #si el valor del actual es mayor al ingresado tonce nos vamos a la izquierda
+            self.padre_y_hermano(valor,actual.get_left()) # y nos fuimos a la izquierda
+        else: #si el valor del actual es mayor al ingresado tonce nos vamos a la derecha
+            self.padre_y_hermano(valor,actual.get_right()) # y nos fuimos a la derecha
+             
     def cantidad_nodos(self,actual): #se puede hacer con inorden
-        if actual is None:
+        if actual is None: #si actual es none tonce retornamos 0
             cant =  0
         else:
-            cant =  1 + self.cantidad_nodos(actual.get_left()) + self.cantidad_nodos(actual.get_right())
-        return cant
+            cant =  1 + self.cantidad_nodos(actual.get_left()) + self.cantidad_nodos(actual.get_right()) #la cantida de nos es 1(raiz) + la cantidad de nodos para la izquierda de la raiz + la cantidad de nodos para la derecha de la raiz
+        return cant #retorno cant
     
     def sucesores(self,valor,actual):
-        if actual is None:
+        if actual is None: #si actual es none tonce retorno none
             return None
-        if actual.get_value() == valor:
-            self.inorden(actual.get_left())
-            self.inorden(actual.get_right())
-        elif actual.get_value() > valor:
-            self.sucesores(valor,actual.get_left())
-        else:
-            self.sucesores(valor,actual.get_right())
+        if actual.get_value() == valor: #si el valor del actual es el que busco
+            self.inorden(actual.get_left()) # funcion que printea nodos a la derecha e izquierda del nodo ingresado por ello le mando la izquierda del que quiero
+            self.inorden(actual.get_right())# funcion que printea nodos a la derecha e izquierda del nodo ingresado por ello le mando la derecha del que quiero
+        elif actual.get_value() > valor: #si el actual es mayor tonce me voy la izquierda
+            self.sucesores(valor,actual.get_left()) # y nos fuimos a la izquierda
+        else: #si el actual es menor tonce me voy la derecha
+            self.sucesores(valor,actual.get_right()) # y nos fuimos a la derecha
      # Recorrido In-orden
     def inorden(self, nodo_actual): #se ocupa en todo
-        if nodo_actual is not None:
-            self.inorden(nodo_actual.get_left())
-            print(nodo_actual.get_value(),end=" ")
-            self.inorden(nodo_actual.get_right())
+        if nodo_actual is not None: #si actual no es none tonce muestro
+            self.inorden(nodo_actual.get_left()) #nodos a su izquierda
+            print(nodo_actual.get_value(),end=" ") # los printeo
+            self.inorden(nodo_actual.get_right()) # nodos a su derecha
             
     def preorden(self, nodo_actual):
-        if nodo_actual is not None:
-            print(nodo_actual.get_value(), end=" ")
-            self.preorden(nodo_actual.get_left())
-            self.preorden(nodo_actual.get_right())
+        if nodo_actual is not None: #si actual no es none tonce muestro
+            print(nodo_actual.get_value(), end=" ") # los printeo
+            self.preorden(nodo_actual.get_left()) #nodos a su izquierda
+            self.preorden(nodo_actual.get_right()) # nodos a su derecha
     # Recorrido Post-orden
     def postorden(self, nodo_actual):
-        if nodo_actual is not None:
-            self.postorden(nodo_actual.get_left())
-            self.postorden(nodo_actual.get_right())
-            print(nodo_actual.get_value(), end=" ")
+        if nodo_actual is not None: #si actual no es none tonce muestro
+            self.postorden(nodo_actual.get_left()) #nodos a su izquierda
+            self.postorden(nodo_actual.get_right()) # nodos a su derecha
+            print(nodo_actual.get_value(), end=" ") # los printeo
 if __name__ == '__main__':
     arbol = Arbol_binario_busqueda()
     valores = [50,30,70,20,40,60,80,10,5]
@@ -234,8 +230,8 @@ if __name__ == '__main__':
             50 es padre de 5?:{arbol.hijo(50,5,arbol.get_raiz())}
             50 es padre de 30?:{arbol.hijo(30,50,arbol.get_raiz())}
             """)
-    camino = arbol.camino(50,10,arbol.get_raiz())
-    print(f"camino de {50} a {10}")
+    camino = arbol.camino(50,70,arbol.get_raiz())
+    print(f"camino de {50} a {70}")
     if camino is not None:
         for valor in camino:
             print(valor,end=" ")
